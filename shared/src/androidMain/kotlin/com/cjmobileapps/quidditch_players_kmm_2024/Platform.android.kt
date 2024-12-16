8718fab73eb56d410f873dafad615ef64a99bf01
Chris Jones <chris@cjmobileapps.com>
2024-12-15 20:36:51 -0600

    first commit

---
+++ shared/src/androidMain/kotlin/com/cjmobileapps/quidditch_players_kmm_2024/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.cjmobileapps.quidditch_players_kmm_2024
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
